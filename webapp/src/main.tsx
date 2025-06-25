9ec838ee9ac239eb20029b6aff8c263a155eb384
alashchev17 <andrew.lashchev15@gmail.com>
2025-06-25 10:31:37 +0200

    fix: get_file_chunks and create_file works on the client

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App'
+import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Theme>
+        <App />
+      </Theme>
     </QueryClientProvider>
   </React.StrictMode>
-);
+)
